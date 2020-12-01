582b64ce11a4ab37e4f754cc5bef2f509a747f5a
Alisa <aliska_1210@mail.ru>
2020-12-01 18:03:52 +0300

    Add RectangleCteator

---
+++ Red21_Paint/Figures/RectangleCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Red21_Paint.Figures
+{
+    class RectangleCreator:IFigureCreator
+    {
+        public Figure CreateFigure(Point start, Point end)
+        {
+            List<Point> squarePoints = new List<Point>();
+            squarePoints.Add(new Point(start.X, start.Y));
+            squarePoints.Add(new Point(start.X, end.Y));
+            squarePoints.Add(new Point(end.X, end.Y));
+            squarePoints.Add(new Point(end.X, start.Y));
+
+            return new Figure(squarePoints);
+        }
+    }
+}
