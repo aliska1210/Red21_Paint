f7710d5f9604966fbaab314a4b59a52ef412fe23
NickWhiteman <romanov_nikita@hotmail.com>
2020-12-01 04:14:10 +0300

    Добавил мелкие настройки по дизайну. поправил прямоугольный треугольник.
    Изменил палитру
    Поправил прямоугольный треугольник
    Настроил пикчер бокс под полноэкранный режим
    Добавил условия нажатия левой клавиши мыши

--- Red21_Paint/Form1.cs
+++ Red21_Paint/Form1.cs
@@ -6,81 +6,91 @@ using System.Windows.Forms;
 
 namespace Red21_Paint
 {
-  public partial class Form1 : Form
-  {
-    Bitmap mainBitmap;
-    Bitmap tmpBitmap { get; set; }
-    Graphics graphics;
-    Pen pen;
-    Point point;
-    IFigureCreator figureCreator;
-    bool isFigure;
-    bool isMouseDown;
-
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
-    }
+        Bitmap mainBitmap;
+        Bitmap tmpBitmap { get; set; }
+        Graphics graphics;
+        Pen pen;
+        Point point;
+        IFigureCreator figureCreator;
+        bool isFigure;
+        bool isMouseDown;
 
-
-
-    private void Form1_Load(object sender, EventArgs e)
-    {
-      mainBitmap = new Bitmap(paintSurface.Width, paintSurface.Height);
-      graphics = Graphics.FromImage(mainBitmap);
-      pen = new Pen(Color.Black, 12);
-    }
-
-    private void paintSurface_MouseMove(object sender, MouseEventArgs e)
-    {
-      if (isMouseDown)
-      {
-        pen.EndCap = LineCap.Round;
-        pen.StartCap = LineCap.Round;
-        if (!isFigure)
+        public Form1()
         {
-          graphics.DrawLine(pen, point, e.Location);
-          point = e.Location;         
-          tmpBitmap = (Bitmap)mainBitmap.Clone();
-          paintSurface.Image = mainBitmap;
+            InitializeComponent();
         }
 
-        if (isFigure)
+
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-          tmpBitmap = new Bitmap(mainBitmap);// в tmp сохраняем mainBitmap до начала рисования новой фигуры, чтобы стирать все рисующиеся фигуры при движении (иначе будет заливка)
-          graphics = Graphics.FromImage(tmpBitmap);// отображаем на экране то что рисуется в tmp
-          Figure figure = figureCreator.CreateFigure(point, e.Location);
-          figure.DrawFigure(graphics, pen);// использую свой собственный метод
-          paintSurface.Image = tmpBitmap;
+            mainBitmap = new Bitmap(paintSurface.MaximumSize.Width, paintSurface.MaximumSize.Height);
+            graphics = Graphics.FromImage(mainBitmap);
+            pen = new Pen(, sizePen.Value);
         }
-      }
-    }
 
-    private void paintSurface_MouseDown(object sender, MouseEventArgs e)
-    {
-      isMouseDown = true;
-      point = e.Location;
-    }
+        private void paintSurface_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (isMouseDown)
+                {
+                    pen.EndCap = LineCap.Round;
+                    pen.StartCap = LineCap.Round;
+                    if (!isFigure)
+                    {
+                        graphics.DrawLine(pen, point, e.Location);
+                        point = e.Location;
+                        tmpBitmap = (Bitmap)mainBitmap.Clone();
+                        paintSurface.Image = mainBitmap;
+                    }
 
-    private void paintSurface_MouseUp(object sender, MouseEventArgs e)
-    {
-      isMouseDown = false;
-      //if (isFigure)
-      //{
-      //  mainBitmap = tmpBitmap;
-      //}    
-    }
+                    if (isFigure)
+                    {
+                        tmpBitmap = new Bitmap(mainBitmap);// в tmp сохраняем mainBitmap до начала рисования новой фигуры, чтобы стирать все рисующиеся фигуры при движении (иначе будет заливка)
+                        graphics = Graphics.FromImage(tmpBitmap);// отображаем на экране то что рисуется в tmp
+                        Figure figure = figureCreator.CreateFigure(point, e.Location);
+                        figure.DrawFigure(graphics, pen);// использую свой собственный метод
+                        paintSurface.Image = tmpBitmap;
+                    }
+                }
+            }
+        }
 
-    private void cyrcle_Click(object sender, EventArgs e)
-    {
-      figureCreator = new CircleCreator();
-      isFigure = true;
-    }
+        private void paintSurface_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            point = e.Location;
+        }
 
-    // метод для соединения точек. Написали свой, так как стандартный не работал с List
-    private void pencil_Click(object sender, EventArgs e)
-    {
-      isFigure = false;
+        private void paintSurface_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            if (isFigure)
+            {
+                mainBitmap = tmpBitmap;
+            }
+        }
+
+        private void cyrcle_Click(object sender, EventArgs e)
+        {
+            figureCreator = new CircleCreator();
+            isFigure = true;
+        }
+
+        // метод для соединения точек. Написали свой, так как стандартный не работал с List
+        private void pencil_Click(object sender, EventArgs e)
+        {
+            isFigure = false;
+        }
+
+        private void triangleDraw_Click(object sender, EventArgs e)
+        {
+            figureCreator = new TriangleCreator();
+            isFigure = true;
+        }
+
     }
-  }
 }
