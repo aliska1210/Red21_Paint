5ef05fb269cc1a2f0361505636b4643a686232b4
NickWhiteman <romanov_nikita@hotmail.com>
2020-12-01 21:28:33 +0300

    Add LineBreak

---
+++ Red21_Paint/Figures/LineBreak.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Red21_Paint.Figures 
+{
+    public class LineBreak
+    {
+        public LineBreak(List<Point> points)
+        {
+            Points = points;
+        }
+        public List<Point> Points { get; set; } // поле принимающее точки
+        public void DrawBreakLine(Graphics graphics, Pen pen)
+        {
+            if (Points.Count < 2) return;
+            for (int i = 0; i < Points.Count - 1; i++)
+            {
+                graphics.DrawLine(pen, Points[i], Points[i + 1]);
+            }
+        }
+    }
+}
