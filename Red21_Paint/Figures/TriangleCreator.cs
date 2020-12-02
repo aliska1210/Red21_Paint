238b27f29b4708fdf3d7b1c57dd4a20613b887cf
Frendor <frendor.dm@gmail.com>
2020-12-02 22:31:13 +0300

    Triangle fix

--- Red21_Paint/Figures/TriangleCreator.cs
+++ Red21_Paint/Figures/TriangleCreator.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Red21_Paint.Figures
 {
@@ -11,14 +7,10 @@ namespace Red21_Paint.Figures
     {
         public Figure CreateFigure(Point start, Point end)
         {
-            int cx = start.X;
-            int cy = start.Y;
-
             List<Point> trianglePoints = new List<Point>();
-            trianglePoints.Add(new Point(cx, cy)); 
+            trianglePoints.Add(new Point((end.X - start.X)/2 + start.X, start.Y)); 
             trianglePoints.Add(new Point(end.X, end.Y)); 
-            trianglePoints.Add(new Point(cx, end.Y));
-            trianglePoints.Add(new Point(cx, cy));
+            trianglePoints.Add(new Point(start.X, end.Y));
 
             return new Figure(trianglePoints);
         }
