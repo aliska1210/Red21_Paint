f2eb959664e94647129eb536119a89369bf537a2
Frendor <frendor.dm@gmail.com>
2020-12-14 01:23:46 +0300

    square fix

--- Red21_Paint/FigureCreators/SquareCreator.cs
+++ Red21_Paint/FigureCreators/SquareCreator.cs
@@ -7,23 +7,33 @@ using System.Threading.Tasks;
 
 namespace Red21_Paint.Figures
 {
-    class SquareCreator: IFigureCreator
+  class SquareCreator : IFigureCreator
+  {
+    public Figure CreateFigure(Point start, Point end)
     {
-        public Figure CreateFigure(Point start, Point end)
-        {
-            int dif = start.X - end.X;
-            end.X = start.X + dif;
-            end.Y = start.Y + dif;
-            Point center = new Point(((end.X + start.X)) / 2, (end.Y + start.Y) / 2);
+      int dif = Math.Abs(start.X - end.X);
+      int R = start.X - end.X;
+      if (R >= 0)
+      {
+        end.X = start.X - dif;
+        end.Y = start.Y - dif;
+      }
+      else
+      {
+        end.X = start.X + dif;
+        end.Y = start.Y + dif;
+      }
 
-            List<Point> squarePoints = new List<Point>();
 
-            squarePoints.Add(new Point(start.X, start.Y));
-            squarePoints.Add(new Point(start.X, end.Y));
-            squarePoints.Add(new Point(end.X, end.Y));
-            squarePoints.Add(new Point(end.X, start.Y));
+      List<Point> squarePoints = new List<Point>();
 
-            return new Figure(squarePoints, start, end, center);
-        }
+      squarePoints.Add(new Point(start.X, start.Y));
+      squarePoints.Add(new Point(start.X, end.Y));
+      squarePoints.Add(new Point(end.X, end.Y));
+      squarePoints.Add(new Point(end.X, start.Y));
+
+      Point center = new Point(((end.X + start.X)) / 2, (end.Y + start.Y) / 2);
+      return new Figure(squarePoints, start, end, center);
     }
+  }
 }
